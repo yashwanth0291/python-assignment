9235fddaee1420056b68f8d81e40255302b656c0
yashwanth0291 <yashwanth0291@gmail.com>
2025-08-19 14:39:08 +0530

    Add files via upload

---
+++ assignment1.1.py
@@ -0,0 +1,6 @@
+a=int(input("enter the first number :"))
+b=int(input("enter the second number :"))
+print("addition :",a+b)
+print("subtraction :",a-b)
+print("multiplication :",a*b)
+print("division :",a/b)
---
+++ assignment1.2.py
@@ -0,0 +1,3 @@
+a=input("enter the first name :")
+b=input("enter the second name :")
+print("hello,",a+b,"! welcome to python programing")
